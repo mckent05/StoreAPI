4764398c4143eb574aef665db8f47058eda971b1
mckent05 <atemitope95@gmail.com>
2024-07-22 09:39:09 +0100

    Added controller for query params

--- controllers/products.js
+++ controllers/products.js
@@ -2,15 +2,36 @@ const productModel = require("../models/product")
 
 
 const getProducts = async(req, res) => {
-  const { company, feature} = req.query
-  const queryObject = {}
-  console.log(queryObject)
+  const { company, featured, numericFilters } = req.query
+  let queryObject = {}
+  
   if(company) {
-    queryObject = {
-      ...queryObject,
-      company: company
-    }
+    queryObject.company = company
   }
+  if(featured) {
+    queryObject.featured = featured === "true" ? true: false
+  }
+  if(numericFilters) {
+    const operatorMap =  {
+      '>': '$gt',
+      '>=': '$gte',
+      '=': '$eq',
+      '<': '$lt',
+      '<=': '$lte',
+    }
+
+    const regex =  /\b(<|>|>=|=|<|<=)\b/g;
+    let filters = numericFilters.trim().replace(regex, (match) => `-${operatorMap[match]}-` )
+    const options = ["price", "rating"]
+    filters.split(",").forEach(filter => {
+      const [price, operator, value] = filter.split("-")
+      if(options.includes(price)) {
+        queryObject[price] = { [operator]: value }
+      }
+    })
+    console.log(queryObject)
+  }
+  
   try {
     const products = await productModel.find(queryObject)
     res.status(200).json({ products, length:products.length })
